297b27a82aa96d7ea414710cf962ea78a241d061
ymsk.sky <ymsk.sky.95@gmail.com>
2019-09-20 14:30:42 +0900

    #1 jsonで記事情報取得を最低限実装

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import urllib
+import http.client
 import json
-import sys
-import codecs
 
 def main():
-    print("hello python")
+    connection = http.client.HTTPSConnection("qiita.com", 443)
+    connection.request("GET", "/api/v2/items")
+
+    response = connection.getresponse()
+
+    data = response.read().decode("utf-8")
+    json_data = json.loads(data)
+
+    for num in range(len(json_data)):
+        created_at = json_data[num]['created_at']
+        title = json_data[num]['title']
+        url = json_data[num]['url']
+        user_id = json_data[num]['user']['id']
+        user_name = json_data[num]['user']['name']
+        
+        print(title)
 
 if __name__ == "__main__":
     main()
